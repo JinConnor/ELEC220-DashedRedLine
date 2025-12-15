46436d2d4392f3baeaafa919fe11194b172bedb4
JinConnor <connorsjin@gmail.com>
2025-12-15 18:01:13 -0500

    working copies of python code

---
+++ analysis.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+gemini = pd.read_json("popularity_buckets_Gemini.json")
+claude = pd.read_json("popularity_classifications_Claude.json")
+gpt = pd.read_json("popularity_classification_GPT.json")
+
