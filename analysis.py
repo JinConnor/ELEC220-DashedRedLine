7d7b44b5cda7a9b1bfff7471e44cb43948d14805
JinConnor <connorsjin@gmail.com>
2025-12-15 18:11:48 -0500

    added popularity json and todo for analysis

--- analysis.py
+++ analysis.py
@@ -4,3 +4,4 @@ gemini = pd.read_json("popularity_buckets_Gemini.json")
 claude = pd.read_json("popularity_classifications_Claude.json")
 gpt = pd.read_json("popularity_classification_GPT.json")
 
+# TOOD: add code to compare the popularity predictions with the actual popularities, along with all the other results we need for the report
