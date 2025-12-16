892fcd3450ec0a5a801420fdf43fe0f1827ed562
Yunxi He <85769753+BlahbousB@users.noreply.github.com>
2025-12-15 22:47:21 -0600

    Added analysis of popularity buckets section
    Uses pandas and sklearn to calculate various error statistics between the models and real song data

--- analysis.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-gemini = pd.read_json("popularity_buckets_Gemini.json")
-claude = pd.read_json("popularity_classifications_Claude.json")
-gpt = pd.read_json("popularity_classification_GPT.json")
-
-# TOOD: add code to compare the popularity predictions with the actual popularities, along with all the other results we need for the report
